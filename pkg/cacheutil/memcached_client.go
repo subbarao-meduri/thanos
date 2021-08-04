fe1ffeb2763ed5c85c16f27d8da0910280651ab8
Chunlin Yang <chuyang@redhat.com>
2021-08-04 15:28:25 +0800

    use thanos 0.22 and enable e2e in KinD on prow (#23)
    * use thanos 0.22 and enable e2e in KinD on prow
    
    Signed-off-by: clyang82 <chuyang@redhat.com>
    
    * cannot unmarshal DNS message
    
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- pkg/cacheutil/memcached_client.go
+++ pkg/cacheutil/memcached_client.go
@@ -223,7 +223,7 @@ func newMemcachedClient(
 	dnsProvider := dns.NewProvider(
 		logger,
 		extprom.WrapRegistererWithPrefix("thanos_memcached_", reg),
-		dns.GolangResolverType,
+		dns.MiekgdnsResolverType,
 	)
 
 	c := &memcachedClient{
